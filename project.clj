113d0e5016a4853bd267f1672f2b04182150c949
Colin Jones <trptcolin@gmail.com>
2016-09-14 21:41:12 -0500

    Bump Clojure version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject macro-workshop "0.1.0-SNAPSHOT"
-  :description "A Clojure macros workshop, presented at Clojure/West 2015"
+  :description "A Clojure macros workshop"
   :url "http://clojuremacros.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[speclj "3.1.0"]]
   :profiles {:dev {:dependencies [[speclj "3.1.0"]]}}
   :source-paths ["doc" "src"]
